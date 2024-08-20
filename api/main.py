3082ac881e3ffa6b95735e7fff9fd8726c501ce6
Giulliano Bueno <giubueno@gmail.com>
2024-08-20 12:42:51 +0200

    Add probes and Dockerfile

--- api/main.py
+++ api/main.py
@@ -9,14 +9,22 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/probes/ready")
+def ready_probe():
+    return {"status": "ready"}
+
+@app.get("/probes/live")
+def live_probe():
+    return {"status": "live"}
+
 @app.get("/sources/{source_id}")
-def read_item(source_id: int, q: Union[str, None] = None):
+def read_source(source_id: int, q: Union[str, None] = None):
     return {"source_id": source_id, "q": q}
 
 @app.get("/languages/{language_id}")
-def read_item(language_id: int, q: Union[str, None] = None):
+def read_language(language_id: int, q: Union[str, None] = None):
     return {"language_id": language_id, "q": q}
 
 @app.get("/events/{event_id}")
-def read_item(event_id: int, q: Union[str, None] = None):
+def read_event(event_id: int, q: Union[str, None] = None):
     return {"event_id": event_id, "q": q}
