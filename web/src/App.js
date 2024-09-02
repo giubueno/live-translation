d9ae14f07348cef9d082bcd55f2869adc061ae10
Giulliano Bueno <giubueno@gmail.com>
2024-09-02 19:14:19 +0200

    Super clean up

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Event from './pages/Event';
-import Home from './pages/Home';
-import Translation from './pages/Translation';
-import NotFound from './pages/NotFound';
+import React, {useState, useEffect} from 'react';
+import { Grid, FormControl, FormLabel, Radio, RadioGroup, FormControlLabel } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+const URL = 'https://api.aboa.today';
 
 function App() {
+  const [language, setLanguage] = useState('english');
+  const [text, setText] = useState();
+
+  const handleRadioChange = (event) => {
+    setLanguage(event.target.value);
+  };
+  
+  // Fetch data from API every 5 seconds
+// Fetch data from API every 5 seconds
+useEffect(() => {
+  const fetchData = () => {
+    fetch(`${URL}/translations/${language}`)
+      .then(response => response.json())
+      .then(data => setText(data));
+  };
+
+  fetchData(); // Initial fetch
+
+  const intervalId = setInterval(fetchData, 5000); // Fetch every 5 seconds
+
+  return () => clearInterval(intervalId); // Cleanup interval on unmount
+}, [language]);
+
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Event />} />
-          <Route path="/translations/:language" element={<Translation />} />
-          <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Grid container spacing={0.5}>
+      <Grid columns={{ xs: 4, sm: 4, md: 4, lg: 4 }}>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Language:</FormLabel>
+          <RadioGroup 
+            aria-labelledby="demo-radio-buttons-group-label" 
+            defaultValue="english" 
+            name="radio-buttons-group"
+            onChange={handleRadioChange}
+          >
+            <FormControlLabel value="english" control={<Radio />} label="English" />
+            <FormControlLabel value="turkish" control={<Radio />} label="Turkish" />
+            <FormControlLabel value="arabic" control={<Radio />} label="Arabic" />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid columns={{ xs: 8, sm: 8, md: 8, lg: 8 }}>
+        <Typography variant="body1" gutterBottom>{text}</Typography>
+      </Grid>
+    </Grid>
   );
 }
 
