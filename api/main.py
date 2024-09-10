695b907bea34f8af768decc7d2b452eeef60f62d
Giulliano Bueno <giubueno@gmail.com>
2024-09-11 01:06:44 +0200

    Use Redis to handle temporary state

--- api/main.py
+++ api/main.py
@@ -3,8 +3,16 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from datetime import datetime
+import redis
+import os
+import json
+
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 
 app = FastAPI()
+
+cache = redis.Redis(host=REDIS_HOST, port=6379, decode_responses=True)
+cache.set('version', "0.1.5")
 
 # Apply the CORSMiddleware with the most permissive settings
 app.add_middleware(
@@ -14,11 +22,6 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
 )
-
-# Mock data
-
-translations = {
-}
 
 class Message(BaseModel):
     language: str
@@ -30,22 +33,32 @@ class Translation(BaseModel):
     language: str
     languages: List[str]
 
+def get_translations():
+    translations = cache.get('translations')
+    if translations:
+        return json.loads(translations)
+    
+    translations = {}
+    set_translations(translations)
+
+    return translations
+
+def set_translations(translations):
+    cache.set('translations', json.dumps(translations))
+
 @app.get("/")
 def read_root():
-    return {"version": "0.1.3"}
-
-# HEALTH endpoints
-
-@app.get("/health")
-def health_check():
-    return {"status": "healthy"}
+    version = cache.get('version')
+    return {"version": version}
 
 @app.get("/languages")
 def read_languages() -> List[str]:
+    translations = get_translations()
     return list(translations.keys())    
 
 @app.get("/translations/{language}")
 def read_translation(language: str, q: Union[str, None] = None) -> Translation:
+    translations = get_translations()
     if not translations:
         return Translation(language=language, languages=translations.keys(), total=0, messages=[])
 
@@ -62,10 +75,19 @@ def read_translation(language: str, q: Union[str, None] = None) -> Translation:
 
 @app.post("/translations")
 def create_translation(message: Message) -> Message:
+    translations = get_translations()
     if message.language not in translations.keys():
         translations[message.language] = []
     
     # prepend the time to the message with only hh:mm
     message.text = f"{datetime.now().strftime('%H:%M')}: {message.text}"
     translations[message.language].append(message.text)
+    set_translations(translations)
+
     return message
+
+@app.post("/translations/reset")
+def reset_translation() -> Translation:
+    translations = {}
+    set_translations(translations)
+    return Translation(language="", languages=[], total=0, messages=[])
