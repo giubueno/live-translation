dc99a11f9fe5a4b7bd71b020740d2f587efd11df
Giulliano Bueno <giubueno@gmail.com>
2024-09-11 19:43:27 +0200

    Remove useless health check

--- infrastructure/containers.tf
+++ infrastructure/containers.tf
@@ -36,13 +36,6 @@ resource "aws_ecs_task_definition" "api" {
           hostPort      = 80
         }
       ]
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:80/ || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 60
-      }
     }
   ])
 
